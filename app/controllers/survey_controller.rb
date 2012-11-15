80e29e0888630eb224e3af1da9d52c2a8e72ba1a
Adam Elnagger <adam.elnagger@gmail.com>
2012-11-14 21:39:52 -0500

    Initial rails config.

---
+++ app/controllers/survey_controller.rb
@@ -0,0 +1,10 @@
+class SurveyController < ApplicationController
+	def home
+	end
+
+	def help
+	end
+
+	def about
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveyor::Application.config.secret_token = '1861966e82ed26f87b7085a668dedfaf61098aabecab395720bd6c13e8e797d9904cb04057653922376af9f40d67c953eaa1098a9e820df33e37ad880782f31f'
